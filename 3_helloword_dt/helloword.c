163774a5da7d80d23644fa21d7c6ff86554260fc
crz21 <2356578958@qq.com>
2025-06-18 00:19:32 +0800

    修改

--- 3_helloword_dt/helloword.c
+++ 3_helloword_dt/helloword.c
@@ -1,21 +1,40 @@
-// #include <stdio.h>
 #include <linux/init.h>
-#include <linux/kernel.h>
 #include <linux/module.h>
+#include <linux/platform_device.h>
+#include <linux/of_device.h>
 
-static int __init helloword_init(void)
+// 在设备树匹配上的时候调用
+static int test_probe(struct platform_device *pdev)
 {
-    printk(KERN_EMERG "[KERN_ENERG] Hello Module Init\n");
-    printk("[default] Hello Module Init\n");
-
-    return 0;
+    printk(KERN_EMERG  "test_probe\n");
+    return 0; 
 }
 
-static void __exit helloword_exit(void) { printk("[default] Hello Module Exit\n"); }
+// 匹配表
+static const struct of_device_id test_of_match[] = {
+    { .compatible = "walnutpi-test" },//用于匹配设备树compatible
+    { }
+};
 
-module_init(helloword_init);
-module_exit(helloword_exit);
+static struct platform_driver pla_drv = {
+    .driver = {
+        .name = "walnutpi-test",
+        .of_match_table = test_of_match,
+    },
+    .probe = test_probe, 
+};
+
+static int test_init(void)
+{
+    printk(KERN_EMERG   "test_init\n");
+    platform_driver_register(&pla_drv);
+    return 0;
+}
+static void test_exit(void)
+{
+    printk(KERN_EMERG  "test_exit\n");
+    platform_driver_unregister(&pla_drv);
+}
+module_init(test_init);
+module_exit(test_exit);
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("crz");
-MODULE_DESCRIPTION("hello module");
-MODULE_ALIAS("hello_module");
